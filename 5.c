856e1328018a153ad4d33741dd883cb127628404
AarushiGOYAL362 <84665917+AarushiGOYAL362@users.noreply.github.com>
2021-05-25 18:45:54 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,12 @@
+/*calculate division of numbers*/
+#include<stdio.h>
+int main()
+{
+	int a,b,quotient;
+	printf("enter two numbers:");
+	scanf("%d %d",&a,&b);
+	quotient=a/b;
+	printf("quotient: %d",quotient);
+	
+	return 0;
+}
