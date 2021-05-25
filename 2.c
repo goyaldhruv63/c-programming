7c59148670c018771d4bceb5f197b360a0d6278f
AarushiGOYAL362 <84665917+AarushiGOYAL362@users.noreply.github.com>
2021-05-25 18:43:37 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,12 @@
+/*Calculate sum of 2 numbers*/
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("\nenter two no: ");
+	scanf("%d %d",&a, &b);
+	sum=a+b;
+	printf("sum : %d",sum);
+	
+	return 0;
+}
