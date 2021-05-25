d902bbc1a9b160b351fa20292b6e6f5795876085
AarushiGOYAL362 <84665917+AarushiGOYAL362@users.noreply.github.com>
2021-05-25 18:45:17 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,12 @@
+/*calculate multiplication of numbers*/
+#include<stdio.h>
+int main()
+{
+	int a,b,product;
+	printf("enter two numbers:");
+	scanf("%d %d",&a,&b);
+	product=a*b;
+	printf("product of two numbers: %d",product);
+	
+	return 0;
+}
